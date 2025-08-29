e143016c8018034732f9dc89ee6b02ff4ba79771
Cesar Martinez <cesarwamartinez@gmail.com>
2025-08-29 16:07:47 -0500

    comit

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,65 +1,66 @@
 import { Component, inject } from '@angular/core';
-import {FormControl, ReactiveFormsModule, FormBuilder,FormGroup, Form} from '@angular/forms';
+import {
+  FormControl,
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Form,
+} from '@angular/forms';
 import { Validators } from '@angular/forms';
-import {featherUser, featherUserPlus} from "@ng-icons/feather-icons"
+import { featherUser, featherUserPlus } from '@ng-icons/feather-icons';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { GlobalService } from '../../core/services/http.service';
 
 @Component({
   selector: 'app-register',
-  imports: [NgIcon,ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
-  viewProviders: [provideIcons({featherUser, featherUserPlus})]
+  viewProviders: [provideIcons({ featherUser, featherUserPlus })],
 })
+
+
 export class RegisterComponent {
+  form: FormGroup;
 
-
-  form: FormGroup
-
-  __GLOBAL_SERVICE = inject(GlobalService)
+  __GLOBAL_SERVICE = inject(GlobalService);
 
   constructor(private fb: FormBuilder) {
     this.form = fb.group({
-      cedula: "104314886",
-      nombre: "cesar",
-      telefono: "3215668657",
-      correo: "cesarwamartinez@gmail.com",
-      contraseña: "1043134886"
-    })
-
+      cedula: ['', Validators.required],
+      nombre: ['', Validators.required],
+      telefono: ['', Validators.required],
+      correo: ['', Validators.required],
+      contraseña: ['', Validators.required],
+    });
   }
 
-  handleSubmitForm(){
-
-    if(this.form.invalid){
-      alert("Confirma que tus datos esten correctamente")
-      return
+  handleSubmitForm() {
+    if (this.form.invalid) {
+      alert('Confirma que tus datos esten correctamente');
+      return;
     }
 
     const body = {
-      id : this.form.get("cedula")?.value,
-      username: this.form.get("nombre")?.value,
-      phone: this.form.get("telefono")?.value,
-      email: this.form.get("correo")?.value,
-      password: this.form.get("contraseña")?.value
-    }
+      id: this.form.get('cedula')?.value,
+      username: this.form.get('nombre')?.value,
+      phone: this.form.get('telefono')?.value,
+      email: this.form.get('correo')?.value,
+      password: this.form.get('contraseña')?.value,
+    };
 
-    console.log(body)
+    
 
-    this.__GLOBAL_SERVICE.__HTTP.post("http://127.0.0.1:8000/register", body).subscribe({
-      next: (e) => {
-        console.log(e)
-        alert("Usuario registrado exitosamente ")
-        this.__GLOBAL_SERVICE.__ROUTER.navigateByUrl("/login")
-
-      }, error: (e) => {
-        alert(`ocurrio un error ${e}`)
-      }
-    })
-
-
-
+    this.__GLOBAL_SERVICE.__HTTP
+      .post('http://127.0.0.1:8000/register', body)
+      .subscribe({
+        next: (e) => {
+          this.__GLOBAL_SERVICE.__NOTYF.success("uSUARIO REGISTRADO EXITOSAMENTE")
+          this.__GLOBAL_SERVICE.__ROUTER.navigateByUrl('/login');
+        },
+        error: (e) => {
+          this.__GLOBAL_SERVICE.__NOTYF.success("Ocurrio un error al registrar usuario")
+        },
+      });
   }
-
 }
