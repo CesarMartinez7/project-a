41b9424af6220c439147909bae7d6926fdfcba19
Cesar Martinez <cesarwamartinez@gmail.com>
2025-09-10 09:00:47 -0500

    add

--- src/app/pages/data-user/data-user.component.ts
+++ src/app/pages/data-user/data-user.component.ts
@@ -1,4 +1,4 @@
-// Importa los módulos necesarios para crear un formulario reactivo
+
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -11,10 +11,10 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./data-user.component.css']
 })
 export class UserFormComponent implements OnInit {
-  // Define la estructura del formulario
+ 
   userForm: FormGroup;
   
-  // Datos de ejemplo para mapear
+  
   userData = {
     id: 136,
     username: 'luis felipe',
@@ -23,24 +23,24 @@ export class UserFormComponent implements OnInit {
     password: '123456'
   };
 
-  // Inyecta el FormBuilder para construir el formulario
+  
   constructor(private fb: FormBuilder) {
     this.userForm = this.fb.group({
-      // Define cada campo con sus validadores
-      id: [{ value: '', disabled: true }], // El ID está deshabilitado para que no se pueda editar
+      
+      id: [{ value: '', disabled: true }], 
       username: ['', Validators.required],
-      phone: ['', [Validators.required, Validators.pattern('^[0-9]*$')]], // Valida que solo sean números
-      email: ['', [Validators.required, Validators.email]], // Valida el formato de correo
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      email: ['', [Validators.required, Validators.email]], 
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
   ngOnInit(): void {
-    // Cuando el componente se carga, mapea los datos al formulario
+  
     this.userForm.patchValue(this.userData);
   }
 
-  // Método para manejar el envío del formulario
+  
   onSubmit(): void {
     if (this.userForm.valid) {
       console.log('Formulario enviado:', this.userForm.value);
